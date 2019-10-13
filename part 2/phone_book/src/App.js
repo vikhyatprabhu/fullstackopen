549ad30a5a60a89a7a4087683ab1f0221285a642
Vikhyat Prabhu <vikhyat.prabhu@gmail.com>
2019-10-13 14:35:53 +0530

    updates changes for exercises from 2-5

--- part 2/phone_book/src/App.js
+++ part 2/phone_book/src/App.js
@@ -1,83 +1,132 @@
-import React, { useState ,useEffect} from 'react'
-import Persons from './components/Persons'
-import SearchFilter from './components/SearchFilter'
-import PersonForm from './components/PersonForm'
-import personService from './services/personService'
-
-import axios from "axios";
+import React, { useState, useEffect } from "react";
+import Persons from "./components/Persons";
+import SearchFilter from "./components/SearchFilter";
+import PersonForm from "./components/PersonForm";
+import personService from "./services/personService";
+import Notification from "./components/Notification";
 
 const App = () => {
-  const [ persons, setPersons] = useState([]);
-  
-  useEffect(()=>{
-    personService.getAll()
-    .then( persons=>
-      setPersons(persons)
-    )
+  const [persons, setPersons] = useState([]);
+  const [notification, setNotification] = useState(null);
 
-  },[]);
-  const [ newName, setNewName ] = useState('')
-  const [ newPhoneNumber, setNewPhoneNumber ] = useState('')
-  const [filterText,setFilterText]= useState('')
+  useEffect(() => {
+    personService.getAll().then(persons => setPersons(persons));
+  }, []);
+  const [newName, setNewName] = useState("");
+  const [newPhoneNumber, setNewPhoneNumber] = useState("");
+  const [filterText, setFilterText] = useState("");
 
-  const handleSubmit=(event)=>{
+  const handleSubmit = event => {
     event.preventDefault();
-    if(persons.map(person=>person.name).includes(newName)){
-      let person=persons.find(person=>person.name===newName);
-      if(person.number !==newPhoneNumber){
-        if(window.confirm(person.name+" already exists in the phonebook,replace the old number with a new one?")){
-           personService.update(person.id,{...person,number:newPhoneNumber})
-           .then(newPerson=>setPersons(persons.map(currentPerson=> person.id!==currentPerson.id? currentPerson:newPerson)))
+    if (persons.map(person => person.name).includes(newName)) {
+      let person = persons.find(person => person.name === newName);
+      if (person.number !== newPhoneNumber) {
+        if (
+          window.confirm(
+            person.name +
+              " already exists in the phonebook,replace the old number with a new one?"
+          )
+        ) {
+          personService
+            .update(person.id, { ...person, number: newPhoneNumber })
+            .then(newPerson => {
+              setPersons(
+                persons.map(currentPerson =>
+                  person.id !== currentPerson.id ? currentPerson : newPerson
+                )
+              )
+              setNotification({
+                message: `Person '${person.name}' was updated`,
+                type: "success"
+              });
+              setTimeout(() => {
+                setNotification(null);
+              }, 5000);
+
+
+                }
+
+
+            );
         }
-      } else{
-        alert(person.name+" already exists"); 
+      } else {
+        alert(person.name + " already exists");
       }
-    } else{
-    personService.create({name:newName, number:newPhoneNumber}).then(
-      newPerson=>
-      setPersons(persons.concat(newPerson))
-    )
+    } else {
+      personService
+        .create({ name: newName, number: newPhoneNumber })
+        .then(newPerson => {
+          setPersons(persons.concat(newPerson));
+          setNotification(
+            {message:`Person ${newPerson.name} was added`,type:"success"}
+          )
+          setTimeout(() => {
+            setNotification(null)
+          }, 5000)
+        });
     }
-    setNewName('')
-    setNewPhoneNumber('')
-  }
+    setNewName("");
+    setNewPhoneNumber("");
+  };
 
-  const handleNameChange=(event)=>{
+  const handleNameChange = event => {
     setNewName(event.target.value);
-  }
-  const handleNumberChange=(event)=>{
+  };
+  const handleNumberChange = event => {
     setNewPhoneNumber(event.target.value);
-  }
+  };
 
-  const handleFilterTextChange=(event)=>{
+  const handleFilterTextChange = event => {
     setFilterText(event.target.value);
-  }
+  };
 
-  const handlePersonDelete=(id)=>{
-    if(window.confirm("Are you sure you want to delete "+persons.find(person=>person.id===id).name)){
-    
-    personService.remove(id).then(
-      response=>{
-        setPersons(persons.filter(p=>p.id!==id))
-      }
-    )
+  const handlePersonDelete = id => {
+    let person = persons.find(person => person.id === id);
+    if (window.confirm("Are you sure you want to delete " + person.name)) {
+      personService
+        .remove(id)
+        .then(response => {
+          setPersons(persons.filter(p => p.id !== id));
+        })
+        .catch(error => {
+          setNotification({
+            message: `Person '${person.name}' was already removed from server`,
+            type: "error"
+          });
+          setTimeout(() => {
+            setNotification(null);
+          }, 5000);
+          setPersons(persons.filter(p => p.id !== person.id));
+        });
     }
+  };
+
+  let displayList = [...persons];
+  if (filterText.length > 0) {
+    displayList = persons.filter(person => person.name.includes(filterText));
   }
-  let displayList=[...persons]
-  if(filterText.length>0){
-    displayList=persons.filter(person => person.name.includes(filterText))
-  } 
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <SearchFilter filterText={filterText} handleFilterTextChange={handleFilterTextChange}/>
-      <PersonForm newName={newName} 
-                 newPhoneNumber={newPhoneNumber} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange} handleSubmit={handleSubmit}/>
+      <Notification notification={notification} />
+      <SearchFilter
+        filterText={filterText}
+        handleFilterTextChange={handleFilterTextChange}
+      />
+      <PersonForm
+        newName={newName}
+        newPhoneNumber={newPhoneNumber}
+        handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange}
+        handleSubmit={handleSubmit}
+      />
       <h2>Numbers</h2>
-      <Persons persons={displayList} handleDelete={handlePersonDelete}> </Persons> 
+      <Persons persons={displayList} handleDelete={handlePersonDelete}>
+        {" "}
+      </Persons>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ part 2/phone_book/src/components/Notification.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './notification.css'
+const Notification = ({ notification }) => {
+    if (notification === null) {
+      return null
+    }
+  
+    return (
+      <div className={notification.type}>
+        {notification.message}
+      </div>
+    )
+  }
+
+  export default Notification;
